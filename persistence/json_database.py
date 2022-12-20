ebaafc8dc15e2cd76f0a079ea0dae6ed75cb7b97
Mikołaj Garbowski <01178523@pw.edu.pl>
2022-12-20 18:40:15 +0100

    implement JsonDatabase, added documentation

--- persistence/json_database.py
+++ persistence/json_database.py
@@ -1,85 +1,170 @@
 import json
-from typing import TextIO, TypeVar, Optional
+from typing import TextIO, Optional
 
-from core.model import User, Message, FriendRequest, Entity
+from core.model import User, Message, FriendRequest, JsonSerializable
 
-T = TypeVar("T")
-Collection = dict[str, T]
+SerializedCollection = dict[str, dict]
+Collection = dict[str, JsonSerializable]
 
-COLLECTION_NAMES: dict[type, str] = {
+COLLECTION_NAMES_MAPPING: dict[type, str] = {
     User: "users",
     Message: "messages",
     FriendRequest: "friend_requests",
 }
 
 
-def infer_collection_type(collection: Collection) -> type:
-    items = list(collection.values())
-    first_item = items[0]
-    return type(first_item)
-
-
 class JsonDatabase:
+    """Database in a JSON file storing generic collections
+
+    Collection is a dictionary mapping string uuids to entities.
+    Entity is an instance of a model class
+        persisted in the database
+        implementing JsonSerializable(Entity) interface
+
+    File storing the database is assigned in the constructor.
+    Public save methods modify this file in a safe way
+
+    Model classes have names assigned to their corresponding collections in the database,
+    mapping is passed as a contructor parameter
+    """
+
     def __init__(self, db_file: TextIO, collection_name_mapping: dict[type, str]):
+        """Create a new database instance persisting data in the given file
+
+        :param db_file: file to persist data in
+        :param collection_name_mapping: dictionary mapping model classes to collection names in the database
+        """
         JsonDatabase._verify_file(db_file, collection_name_mapping)
         self.__db_file = db_file
         self.__collection_name_mapping = collection_name_mapping
 
-    def get_item(self, item_id: str, item_type: type) -> Optional[Entity]:
-        collection = self.get_collection(item_type)
-        return collection.get(item_id, None)
+    def get_item(self, item_id: str, item_type: type[JsonSerializable]) -> Optional[JsonSerializable]:
+        """Get an entity by its id or None if not found
 
-    def save_item(self, item: Entity) -> Entity:
-        item_type = type(item)
-        collection = self.get_collection(item_type)
-        collection[item.uuid] = item
-        return item
+        :param item_id: id of entity
+        :param item_type: type of searched entity
+        """
+        collection = self._get_serialized_collection(item_type)
+        if item_id not in collection:
+            return None
 
-    def get_collection(self, collection_type: type) -> Collection:
-        collections = self._load_collections()
-        name = self.collection_name(collection_type)
-        return collections[name]
+        item_json = collection[item_id]
+        return item_type.from_json(item_json)
+
+    def save_item(self, item: JsonSerializable) -> None:
+        """Save an entity to the database, overwriting previous value if it existed"""
+        collection_type = type(item)
+        collection_name = self._collection_name(collection_type)
+        collection = self._get_serialized_collection(collection_type)
+        collection[item.uuid] = item.to_json()
+        self._save_serialized_collection(collection, collection_name)
+
+    def get_collection(self, collection_type: type[JsonSerializable]) -> Collection:
+        """Get collection of entities by their type"""
+        serialized_collection = self._get_serialized_collection(collection_type)
+        return _deserialize_collection(serialized_collection, collection_type)
 
     def save_collection(self, collection: Collection, collection_type: Optional[type] = None) -> None:
+        """Save a collection to the database, overwriting all existing items
+
+        :param collection: collection of entities to save in the database
+        :param collection_type: type of the collection in case it is empty
+            by default type is inferred
+            only specify type explicitly when saving an empty collection
+        :raises ValueError: if at the same time collection is empty and type is not specified
+            or the collection is not empty and type is specified
+        """
         if not collection and collection_type is None:
             raise ValueError("If collection is empty, type must be specified")
 
         if collection and collection_type is not None:
             raise ValueError("Collection type can only be specified for empty collections")
 
-        collections = self._load_collections()
-        collection_type = infer_collection_type(collection) if collection else collection_type
-        name = self.collection_name(collection_type)
-        collections[name] = collection
-        self._save_collections(collections)
+        collection_type = _infer_collection_type(collection) if collection else collection_type
+        name = self._collection_name(collection_type)
 
-    def collection_name(self, collection_type: type) -> str:
+        serialized_collection = _serialize_collection(collection)
+        self._save_serialized_collection(serialized_collection, name)
+
+    def _get_serialized_collection(self, collection_type: type[JsonSerializable]) -> SerializedCollection:
+        """Get serialized collection of a given type"""
+        name = self._collection_name(collection_type)
+        serialized_collections = self._load_all_collections()
+        return serialized_collections[name]
+
+    def _save_serialized_collection(
+            self,
+            serialized_collection: SerializedCollection,
+            collection_name: str
+    ) -> None:
+        """Save a serialized collection in the database"""
+        all_collections = self._load_all_collections()
+        all_collections[collection_name] = serialized_collection
+        self._save_all_collections(all_collections)
+
+    def _collection_name(self, collection_type: type) -> str:
+        """Get name mapped to a given type"""
         return self.__collection_name_mapping[collection_type]
 
-    def _load_collections(self) -> dict[str, Collection]:
+    def _load_all_collections(self) -> dict[str, SerializedCollection]:
+        """Load database from file"""
         return json.load(self.__db_file)
 
-    def _save_collections(self, collections: dict[str, Collection]) -> None:
+    def _save_all_collections(self, collections: dict[str, SerializedCollection]) -> None:
+        """Save database to the file"""
         json.dump(collections, self.__db_file)
 
     @staticmethod
     def _verify_file(db_file: TextIO, collection_name_mapping: dict[type, str]) -> None:
         """Verify that given file valid for given mapping of collection names
 
+        File must be readable and writable.
+        File must be in JSON format.
+        JSON must contain all collection names as defined in the mapping
+
         :param db_file: file storing the database
         :param collection_name_mapping: dictionary mapping model classes
             to names of their corresponding collections in the database file
+        :raises InvalidDatabaseFileError: if given file is not valid
         """
-        pass
+        if not db_file.readable() or not db_file.writable():
+            raise InvalidDatabaseFileError()
+
+        try:
+            file_data = json.load(db_file)
+            collection_names = set(collection_name_mapping.values())
+            file_keys = set(file_data.keys())
+            if not collection_names.issubset(file_keys):
+                raise InvalidDatabaseFileError()
+        except Exception as e:
+            raise InvalidDatabaseFileError() from e
 
 
-class FilesystemUserRepository:
-    pass
+def _infer_collection_type(collection: Collection) -> type:
+    """Infer type of items in the collection based on first element"""
+    items = list(collection.values())
+    first_item = items[0]
+    return type(first_item)
 
 
-class FilesystemMessageRepository:
-    pass
+def _serialize_collection(collection: Collection) -> SerializedCollection:
+    """Serialize a collection to JSON format"""
+    return {
+        uuid: item.to_json()
+        for uuid, item in collection.items()
+    }
 
 
-class FilesystemFriendRequestRepository:
+def _deserialize_collection(
+        serialized_collection: SerializedCollection,
+        collection_type: type[JsonSerializable]
+) -> Collection:
+    """Deserialize a collection from JSON format"""
+    return {
+        uuid: collection_type.from_json(item_json)
+        for uuid, item_json in serialized_collection.items()
+    }
+
+
+class InvalidDatabaseFileError(ValueError):
     pass
