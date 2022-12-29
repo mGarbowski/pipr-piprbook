339d27e6759f652a5c0f5ab3591a8489cf00e386
Mikołaj Garbowski <01178523@pw.edu.pl>
2022-12-29 21:27:22 +0100

    fixed profile page styling

--- gui/ui_components/ui_profile_page.py
+++ gui/ui_components/ui_profile_page.py
@@ -18,41 +18,108 @@ class Ui_ProfilePage(object):
         if not ProfilePage.objectName():
             ProfilePage.setObjectName(u"ProfilePage")
         ProfilePage.resize(737, 547)
-        self.bio_display = QLabel(ProfilePage)
-        self.bio_display.setObjectName(u"bio_display")
-        self.bio_display.setGeometry(QRect(10, 110, 241, 71))
-        self.bio_display.setWordWrap(True)
-        self.profile_picture_header = QLabel(ProfilePage)
-        self.profile_picture_header.setObjectName(u"profile_picture_header")
-        self.profile_picture_header.setGeometry(QRect(440, 90, 151, 16))
-        self.bio_input = QTextEdit(ProfilePage)
-        self.bio_input.setObjectName(u"bio_input")
-        self.bio_input.setGeometry(QRect(30, 190, 221, 181))
-        self.pushButton_2 = QPushButton(ProfilePage)
-        self.pushButton_2.setObjectName(u"pushButton_2")
-        self.pushButton_2.setGeometry(QRect(420, 480, 80, 23))
-        self.username_display = QLabel(ProfilePage)
-        self.username_display.setObjectName(u"username_display")
-        self.username_display.setGeometry(QRect(10, 50, 241, 16))
-        self.email_display = QLabel(ProfilePage)
-        self.email_display.setObjectName(u"email_display")
-        self.email_display.setGeometry(QRect(10, 80, 241, 16))
-        self.update_bio_button = QPushButton(ProfilePage)
-        self.update_bio_button.setObjectName(u"update_bio_button")
-        self.update_bio_button.setGeometry(QRect(40, 390, 211, 23))
-        self.pushButton = QPushButton(ProfilePage)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(270, 480, 80, 23))
-        self.upload_profile_picture_button = QPushButton(ProfilePage)
-        self.upload_profile_picture_button.setObjectName(u"upload_profile_picture_button")
-        self.upload_profile_picture_button.setGeometry(QRect(410, 390, 151, 23))
+        self.verticalLayout_4 = QVBoxLayout(ProfilePage)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
         self.profile_header = QLabel(ProfilePage)
         self.profile_header.setObjectName(u"profile_header")
-        self.profile_header.setGeometry(QRect(20, 10, 161, 16))
-        self.profile_picture = QLabel(ProfilePage)
+
+        self.verticalLayout_4.addWidget(self.profile_header)
+
+        self.widget_5 = QWidget(ProfilePage)
+        self.widget_5.setObjectName(u"widget_5")
+        self.horizontalLayout_2 = QHBoxLayout(self.widget_5)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.widget_4 = QWidget(self.widget_5)
+        self.widget_4.setObjectName(u"widget_4")
+        self.verticalLayout_3 = QVBoxLayout(self.widget_4)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.username_display = QLabel(self.widget_4)
+        self.username_display.setObjectName(u"username_display")
+
+        self.verticalLayout_3.addWidget(self.username_display)
+
+        self.email_display = QLabel(self.widget_4)
+        self.email_display.setObjectName(u"email_display")
+
+        self.verticalLayout_3.addWidget(self.email_display)
+
+        self.bio_display = QLabel(self.widget_4)
+        self.bio_display.setObjectName(u"bio_display")
+        self.bio_display.setWordWrap(True)
+
+        self.verticalLayout_3.addWidget(self.bio_display)
+
+
+        self.horizontalLayout_2.addWidget(self.widget_4)
+
+        self.widget_6 = QWidget(self.widget_5)
+        self.widget_6.setObjectName(u"widget_6")
+        self.horizontalLayout_3 = QHBoxLayout(self.widget_6)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.verticalSpacer_2 = QSpacerItem(20, 98, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.horizontalLayout_3.addItem(self.verticalSpacer_2)
+
+
+        self.horizontalLayout_2.addWidget(self.widget_6)
+
+
+        self.verticalLayout_4.addWidget(self.widget_5)
+
+        self.widget_3 = QWidget(ProfilePage)
+        self.widget_3.setObjectName(u"widget_3")
+        self.horizontalLayout = QHBoxLayout(self.widget_3)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.widget = QWidget(self.widget_3)
+        self.widget.setObjectName(u"widget")
+        self.verticalLayout = QVBoxLayout(self.widget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.bio_input = QTextEdit(self.widget)
+        self.bio_input.setObjectName(u"bio_input")
+
+        self.verticalLayout.addWidget(self.bio_input)
+
+        self.update_bio_button = QPushButton(self.widget)
+        self.update_bio_button.setObjectName(u"update_bio_button")
+
+        self.verticalLayout.addWidget(self.update_bio_button)
+
+
+        self.horizontalLayout.addWidget(self.widget)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
+        self.widget_2 = QWidget(self.widget_3)
+        self.widget_2.setObjectName(u"widget_2")
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.widget_2.sizePolicy().hasHeightForWidth())
+        self.widget_2.setSizePolicy(sizePolicy)
+        self.upload_profile_picture_button = QPushButton(self.widget_2)
+        self.upload_profile_picture_button.setObjectName(u"upload_profile_picture_button")
+        self.upload_profile_picture_button.setGeometry(QRect(30, 210, 138, 23))
+        self.profile_picture = QLabel(self.widget_2)
         self.profile_picture.setObjectName(u"profile_picture")
-        self.profile_picture.setGeometry(QRect(430, 210, 59, 15))
+        self.profile_picture.setGeometry(QRect(9, 9, 189, 192))
+        sizePolicy1 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.profile_picture.sizePolicy().hasHeightForWidth())
+        self.profile_picture.setSizePolicy(sizePolicy1)
         self.profile_picture.setScaledContents(True)
+
+        self.horizontalLayout.addWidget(self.widget_2)
+
+
+        self.verticalLayout_4.addWidget(self.widget_3)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout_4.addItem(self.verticalSpacer)
+
 
         self.retranslateUi(ProfilePage)
 
@@ -61,15 +128,12 @@ class Ui_ProfilePage(object):
 
     def retranslateUi(self, ProfilePage):
         ProfilePage.setWindowTitle(QCoreApplication.translate("ProfilePage", u"Form", None))
-        self.bio_display.setText(QCoreApplication.translate("ProfilePage", u"Bio display", None))
-        self.profile_picture_header.setText(QCoreApplication.translate("ProfilePage", u"Profile picture", None))
-        self.pushButton_2.setText(QCoreApplication.translate("ProfilePage", u"PushButton", None))
+        self.profile_header.setText(QCoreApplication.translate("ProfilePage", u"Profile header", None))
         self.username_display.setText(QCoreApplication.translate("ProfilePage", u"Username display", None))
         self.email_display.setText(QCoreApplication.translate("ProfilePage", u"Email display", None))
+        self.bio_display.setText(QCoreApplication.translate("ProfilePage", u"Bio display", None))
         self.update_bio_button.setText(QCoreApplication.translate("ProfilePage", u"Update bio", None))
-        self.pushButton.setText(QCoreApplication.translate("ProfilePage", u"PushButton", None))
         self.upload_profile_picture_button.setText(QCoreApplication.translate("ProfilePage", u"Upload profile picture", None))
-        self.profile_header.setText(QCoreApplication.translate("ProfilePage", u"Profile header", None))
         self.profile_picture.setText("")
     # retranslateUi
 
