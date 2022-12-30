c7a32fa2f3512417106c2957756baf0057af954b
Mikołaj Garbowski <01178523@pw.edu.pl>
2022-12-30 20:37:11 +0100

    extracted superclass BaseRepository from User, Message, FriendRequest and Photo repositories

--- persistence/repositories.py
+++ persistence/repositories.py
@@ -1,12 +1,55 @@
 """Repository classes for accessing data persisted in a Database"""
+from abc import ABC
+from typing import Optional, List, TypeVar
 
-from typing import Optional, List
-
-from core.model import User, Message, FriendRequest, Photo
+from core.model import User, Message, FriendRequest, Entity
 from persistence.interface import Database, JsonSerializer
 
+T = TypeVar("T", bound=Entity)
 
-class UserRepository:
+
+class BaseRepository(ABC):
+    def __init__(
+            self,
+            database: Database,
+            serializer: JsonSerializer,
+            collection_name: str
+    ):
+        """Create a Repository connected to the given database
+
+        :param database: database to persist entities in
+        :param serializer: JSON serializer for entities
+        :param collection_name: collection name in the database
+        """
+        self._database = database
+        self._serializer = serializer
+        self._collection_name = collection_name
+
+    def save(self, entity: T):
+        """Create new entity or update existing one
+
+        :param entity: entity to create or update
+        """
+        entity_dict = self._serializer.to_json(entity)
+        self._database.save(entity_dict, self._collection_name)
+
+    def get_by_id(self, entity_id: str) -> Optional[T]:
+        """Get entity by id or None if it does not exist
+
+        :param entity_id: id of searched entity
+        """
+        entity_dict = self._database.get_by_id(entity_id, self._collection_name)
+        return self._serializer.from_json(entity_dict) if entity_dict else None
+
+    def delete(self, entity: T):
+        """Delete entity or do nothing if it does not exist in the database
+
+        :param entity: entity to delete
+        """
+        self._database.delete_by_id(entity.uuid, self._collection_name)
+
+
+class UserRepository(BaseRepository):
     """Class for accessing users stored in a database"""
 
     def __init__(
@@ -21,38 +64,13 @@ class UserRepository:
         :param serializer: JSON serializer for users
         :param collection_name: name of collection in datbase, defaults to "users"
         """
-        self.__database = database
-        self.__serializer = serializer
-        self.__collection_name = collection_name
+        super().__init__(database, serializer, collection_name)
 
     def get_all(self) -> List[User]:
         """Get all users"""
-        users_json = self.__database.get_collection(self.__collection_name)
-        users = [self.__serializer.from_json(user_json) for user_json in users_json]
+        users_json = self._database.get_collection(self._collection_name)
+        users = [self._serializer.from_json(user_json) for user_json in users_json]
         return users
-
-    def save(self, user: User):
-        """Create new user or update existing one
-
-        :param user: user to create or update
-        """
-        user_dict = self.__serializer.to_json(user)
-        self.__database.save(user_dict, self.__collection_name)
-
-    def get_by_id(self, user_id: str) -> Optional[User]:
-        """Get user by id or None if it does not exist
-
-        :param user_id: id of searched user
-        """
-        user_dict = self.__database.get_by_id(user_id, self.__collection_name)
-        return self.__serializer.from_json(user_dict) if user_dict is not None else None
-
-    def delete(self, user: User) -> None:
-        """Delete user
-
-        :param user: user to delete
-        """
-        self.__database.delete_by_id(user.uuid, self.__collection_name)
 
     def get_by_username(self, username: str) -> Optional[User]:
         """Get user by username or None if not found
@@ -85,7 +103,7 @@ class UserRepository:
         ]
 
 
-class MessageRepository:
+class MessageRepository(BaseRepository):
     """Class for accessing messages persisted in a database"""
 
     def __init__(
@@ -100,26 +118,7 @@ class MessageRepository:
         :param serializer: JSON serializer for messages
         :param collection_name: name of collection in datbase, defaults to "messages"
         """
-        self.__database = database
-        self.__serializer = serializer
-        self.__collection_name = collection_name
-
-    def save(self, message: Message):
-        """Create new message or update existing one"""
-        message_json = self.__serializer.to_json(message)
-        self.__database.save(message_json, self.__collection_name)
-
-    def get_by_id(self, message_id: str) -> Optional[Message]:
-        """Get message by id or None if it does not exist
-
-        :param message_id: id of searched message
-        """
-        message_json = self.__database.get_by_id(message_id, self.__collection_name)
-        return self.__serializer.from_json(message_json) if message_json else None
-
-    def delete(self, message: Message):
-        """Delete message or do nothing if it does not exist in the database"""
-        self.__database.delete_by_id(message.uuid, self.__collection_name)
+        super().__init__(database, serializer, collection_name)
 
     def get_messages(self, user_a: User, user_b: User) -> List[Message]:
         """Get all messages exchanged between two users, ordered by timestamp
@@ -130,8 +129,8 @@ class MessageRepository:
         :param user_a: one of the users sending or receiving messages
         :param user_b: one of the users sending or receiving messages
         """
-        messages_json = self.__database.get_collection(self.__collection_name)
-        messages = [self.__serializer.from_json(message_json) for message_json in messages_json]
+        messages_json = self._database.get_collection(self._collection_name)
+        messages = [self._serializer.from_json(message_json) for message_json in messages_json]
         messages = [msg for msg in messages if _is_message_matched(msg, user_a, user_b)]
         messages = sorted(messages, key=lambda msg: msg.timestamp)
         return messages
@@ -143,7 +142,7 @@ def _is_message_matched(message: Message, user_a: User, user_b: User) -> bool:
     return message.to_user_id in user_ids and message.from_user_id in user_ids
 
 
-class FriendRequestRepository:
+class FriendRequestRepository(BaseRepository):
     """Class for accessing friend requests stored in a database"""
 
     def __init__(
@@ -158,32 +157,7 @@ class FriendRequestRepository:
         :param serializer: JSON serializer for friend requests
         :param collection_name: collection name in the database, default to "friend_requests"
         """
-        self.__database = database
-        self.__serializer = serializer
-        self.__collection_name = collection_name
-
-    def save(self, friend_request: FriendRequest):
-        """Create new friend request or update existing one
-
-        :param friend_request: friend request to save or update
-        """
-        request_dict = self.__serializer.to_json(friend_request)
-        self.__database.save(request_dict, self.__collection_name)
-
-    def get_by_id(self, friend_request_id: str) -> Optional[FriendRequest]:
-        """Get friend request by id or None if it does not exist
-
-        :param friend_request_id: id of searched friend request
-        """
-        request_dict = self.__database.get_by_id(friend_request_id, self.__collection_name)
-        return self.__serializer.from_json(request_dict) if request_dict else None
-
-    def delete(self, friend_request: FriendRequest):
-        """Delete friend request or do nothing if it does not exist in the database
-
-        :param friend_request: friend request to delete
-        """
-        self.__database.delete_by_id(friend_request.uuid, self.__collection_name)
+        super().__init__(database, serializer, collection_name)
 
     def get_requests_to_user(self, to_user: User) -> List[FriendRequest]:
         """Get all requests sent to given user, ordered by timestamp
@@ -207,50 +181,23 @@ class FriendRequestRepository:
 
     def _get_all_requests(self) -> List[FriendRequest]:
         """Get all friend requests from the database"""
-        requests_json = self.__database.get_collection(self.__collection_name)
-        return [self.__serializer.from_json(req_json) for req_json in requests_json]
+        requests_json = self._database.get_collection(self._collection_name)
+        return [self._serializer.from_json(req_json) for req_json in requests_json]
 
 
-class PhotoRepository:
+class PhotoRepository(BaseRepository):
     """Class for accessing photos stored in a database"""
 
     def __init__(
             self,
             database: Database,
-            serializer: JsonSerializer[Photo],
+            serializer: JsonSerializer,
             collection_name: str = "photos"
     ):
-        """Create a PhotoRepository connected to the given database
+        """Create PhotoRepository connected to the given database
 
         :param database: database to persist photos in
         :param serializer: JSON serializer for photos
         :param collection_name: collection name in the database, defaults to "photos"
         """
-        self.__database = database
-        self.__serializer = serializer
-        self.__collection_name = collection_name
-
-    def save(self, photo: Photo):
-        """Create new photo or update existing one
-
-        :param photo: photo to create or update
-        """
-        photo_dict = self.__serializer.to_json(photo)
-        self.__database.save(photo_dict, self.__collection_name)
-
-    def get_by_id(self, photo_id: str) -> Optional[Photo]:
-        """Get photo by id or None if it does not exist
-
-        :param photo_id: id of searched photo
-        """
-        photo_dict = self.__database.get_by_id(photo_id, self.__collection_name)
-        return self.__serializer.from_json(photo_dict) if photo_dict else None
-
-    def delete(self, photo: Photo):
-        """Delete photo or do nothing if it does not exist in the database
-
-        :param photo: photo to delete
-        """
-        self.__database.delete_by_id(photo.uuid, self.__collection_name)
-
-# TODO: factor out an abstract class
+        super().__init__(database, serializer, collection_name)
