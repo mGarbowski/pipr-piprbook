98cd803277412742c931b32858c0f29e49f3ed65
Mikołaj Garbowski <01178523@pw.edu.pl>
2022-12-30 17:47:25 +0100

    added documentation for persistence.interface

--- persistence/interface.py
+++ persistence/interface.py
@@ -1,31 +1,45 @@
+"""Protocol classes (interfaces) used by the persistence layer"""
+
 from typing import Protocol, TypeVar, Optional, Dict, List
 
-T = TypeVar("T", covariant=True)
+from core.model import Entity
+
+T = TypeVar("T", bound=Entity, covariant=True)
 
 
 class Database(Protocol):
+    """Interface for a database storing collections of entity dictionaries"""
+
     def save(self, entity_dict: Dict, collection_name: str):
+        """Create new entity or update existing"""
         ...
 
     def get_by_id(self, entity_id: str, collection_name: str) -> Optional[Dict]:
+        """Get entity dictionary by id or None if it does not exist"""
         ...
 
     def delete_by_id(self, entity_id: str, collection_name: str):
+        """Delete entity by its id or do nothing if it does not exist"""
         ...
 
-    def save_collection(self, collection: List[dict], collection_name: str):
+    def save_collection(self, collection: List[Dict], collection_name: str):
+        """Save a collection of entity dictionaries, overwriting all existing"""
         ...
 
-    def get_collection(self, collection_name: str) -> List[dict]:
+    def get_collection(self, collection_name: str) -> List[Dict]:
+        """Get collection of entity dictionaries by name"""
         ...
 
 
 class JsonSerializer(Protocol[T]):
+    """Generic interface for JSON serializer / deserializer of model classes"""
 
     @staticmethod
     def to_json(entity) -> Dict:
+        """Serialize entity to its JSON-like dictionary representation"""
         ...
 
     @staticmethod
     def from_json(json_dict: Dict) -> T:
+        """Deserialize entity from its JSON-like dictionary representation"""
         ...
